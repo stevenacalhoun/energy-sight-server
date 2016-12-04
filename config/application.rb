5b970b8acc8c470e680d6f6621c610fe8fe94900
stevenacalhoun <stevenacalhoun@gmail.com>
2016-12-04 01:13:04 -0500

    Master script

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,8 @@ module MusicThermometer
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
 
-    RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET'])
-    
+    # RSpotify::authenticate(ENV['SPOTIFY_CLIENT_ID'], ENV['SPOTIFY_SECRET'])
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
