409eda72f8c2fd87c260a361a0a8ea5d4ebe9fb5
stevenacalhoun <stevenacalhoun@gmail.com>
2017-01-09 19:57:47 -0600

    Change in song data return

--- app/controllers/artist_info_controller.rb
+++ app/controllers/artist_info_controller.rb
@@ -7,8 +7,37 @@ class ArtistInfoController < ApplicationController
     earliestDate = Date.parse(params[:startDate])
     latestDate = Date.parse(params[:endDate])
 
-    results = TopChart.select('top_charts.chart_week, top_charts.country, songs.spotify_id, songs.artist, songs.title, top_charts.rank, songs.preview_url, songs.album_art_link').joins(:song).where('top_charts.chart_week' => earliestDate..(latestDate+7)).where('songs.artist' => artist)
+    songs = TopChart.select('top_charts.chart_week, top_charts.country, songs.spotify_id, songs.artist, songs.title, top_charts.rank, songs.preview_url, songs.album_art_link').joins(:song).where('top_charts.chart_week' => earliestDate..(latestDate+7)).where('songs.artist' => artist)
+
+    songData = {}
+    songs.each do |song|
+      if songData.key?(song.title) == false
+        songData[song.title] = {
+          "spotify_id": song.spotify_id,
+          "title": song.title,
+          "artist": song.artist,
+          "preview_url": song.preview_url,
+          "album_art_link": song.album_art_link,
+          "dates": []
+        }
+      end
+      songData[song.title][:dates].push({
+          "chart_week": song.chart_week,
+          "rank": song.rank,
+          "country": song.country
+        })
+    end
+
+    results = flattenDict(songData).sort_by{|obj| obj[:title]}
 
     render json: results
   end
+end
+
+def flattenDict(dict)
+  array = []
+  dict.each do |key, data|
+    array.append(data)
+  end
+  return array
 end
